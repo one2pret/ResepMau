054b65dc6be28d9392ba088b229c87c5c7cb7687
NineLota <ninelota@localhost.localdomain>
2019-01-11 02:02:10 +0700

    post

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Tambah Resep
+                <div class="card-header">Edit Resep
                   </div>
                   <form class="" action="{{route('update',$post)}}" method="post">
                     @csrf
